70dcd84404f4fc0eba6050e811b3acdc6b8e14e6
ioan_davies <ioan3.davies@live.uwe.ac.uk>
2023-02-05 04:11:36 +0000

    Death screen _ death system added

---
+++ Assets/DeathMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    public void Retry()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,29 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
     public AudioSource missSFX;
+
     public TMPro.TextMeshPro scoreText;
+    public TMPro.TextMeshPro lifeText;
+
     static int comboScore;
+    static int lifeScore = 50;
+
     void Start()
     {
         Instance = this;
         comboScore = 0;
+        lifeScore = 50;
     }
     public static void Hit()
     {
         comboScore += 1;
+        lifeScore += 1;
     }
     public static void Miss()
     {
         comboScore = 1;
+        lifeScore -= 2;
+
+        if (lifeScore < 0)
+        {
+            lifeScore = 0;
+            SceneManager.LoadScene(3);
+
+        }
+
         Instance.missSFX.Play();
     }
     private void Update()
     {
         scoreText.text = comboScore.ToString();
+        lifeText.text = lifeScore.ToString();
     }
 }
