680649578786266adeb2aa8127482d9794b40324
ioan_davies <ioan3.davies@live.uwe.ac.uk>
2023-02-05 09:13:07 +0000

    New scenes added

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Spring()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Summer()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void Autumn()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void Winter()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
